2480dcc35d1c3108cf3d17490b11be179c9b9b0c
Shreyas Singh <shreyassingh.official@gmail.com>
2023-11-19 22:55:58 +0530

    Force set theme to dark

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <Provider>
-      <html lang="en">
+      <html lang="en" className="dark">
         <body className={inter.className}>
           <Header />
           {children}
